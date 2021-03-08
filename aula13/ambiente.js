a8e0268bf65c7610466b9b8175931de014e6197d
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-03-08 13:04:54 -0300

    Create ambiente.js to practice repetition structures

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+// Repetição com teste no início (WHILE)
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// Repetição com teste no final (DO WHILE)
+var d = 1
+do {
+    console.log(`Passo ${d}`)
+    c++
+} while (c <= 6)
