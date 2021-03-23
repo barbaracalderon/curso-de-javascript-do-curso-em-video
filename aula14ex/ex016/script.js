1bd582ced9dc0de9f61d2d1c1017ed1db484c99d
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-03-23 10:51:44 -0300

    Created the logic behind counting numbers with a given step

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,11 +1,34 @@
 function contar() {
-    var inicio = document.querySelector('div#txtinicio')
-    var fim = document.querySelector('div#txtfim')
-    var passo = document.querySelector('div#txtpasso')
-    var res = document.querySelector('div#res')
+    let inicio = document.getElementById('txtinicio')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpasso')
+    let res = document.getElementById('res')
 
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length ==  0) {
+        window.alert('ERRO: Verifique os dados e tente novamente.')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo = 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
 
-    res.style.textAlign = 'center'
-    res.innerHTML = `Você selecionou início em ${inicio}, fim em ${fim} e passo de ${passo}.`
+    }
 
 }
